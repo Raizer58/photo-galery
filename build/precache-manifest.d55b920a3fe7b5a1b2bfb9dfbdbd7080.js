dd3590ce774478e7be12e4c38b04db411aadfcda
Raizer58 <Raizer9058w@gmail.com>
2020-07-25 04:22:03 +0300

    feat: deploy

---
+++ build/precache-manifest.d55b920a3fe7b5a1b2bfb9dfbdbd7080.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dc3d8756482bf0fac9a5e10a924c85b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4c9537b8dae854d8432",
+    "url": "/static/js/2.da051001.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.da051001.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de024dd75cd8aebf1e75",
+    "url": "/static/js/main.62f29f27.chunk.js"
+  },
+  {
+    "revision": "802e617212e2ccec7a16",
+    "url": "/static/js/runtime-main.262fef20.js"
+  }
+]);
